937f60056f72c39709c637d4f8b7705b6cc5c5e6
Yash Navadiya <yashnavadiya12@gmail.com>
2021-12-08 00:20:17 -0500

    ConfigureSystem

--- db4odemo/src/Business/ConfigureASystem.java
+++ db4odemo/src/Business/ConfigureASystem.java
@@ -1,7 +1,7 @@
 package Business;
 
 //import Business.Employee.Employee;
-import Business.Role.SystemAdminRole;
+import Business.Role.SysAdminRole;
 import Business.UserAccount.UserAcc;
 import com.db4o.User;
 
@@ -13,19 +13,12 @@ public class ConfigureASystem {
     
     public static EcoSystem configure(){
         
-//        EcoSystem system = EcoSystem.getInstance();
-//        
-//        //Create a network
-//        //create an enterprise
-//        //initialize some organizations
-//        //have some employees 
-//        //create user account
 
 EcoSystem system = EcoSystem.getInstance();
         
-        Business.User.User user = system.getUserDirectory().createUser("AUTO");       
+        Business.User.User user = system.getUserDir().createUser("AUTO");       
         
-        UserAcc ua = system.getUserAccDir().createUserAccount("admin", "admin", user, new SystemAdminRole());
+        UserAcc ua = system.getUserAccDir().createUserAcc("admin", "admin", user, new SysAdminRole());
         
         return system;
     }
