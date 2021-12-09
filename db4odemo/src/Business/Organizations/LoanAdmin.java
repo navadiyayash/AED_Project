4e81ae1f46cdedb3caa5dce62c582fa32b63dda0
Yash Navadiya <yashnavadiya12@gmail.com>
2021-12-09 12:46:57 -0500

    updated LoanAdmin

--- db4odemo/src/Business/Organizations/LoanAdmin.java
+++ db4odemo/src/Business/Organizations/LoanAdmin.java
@@ -17,5 +17,11 @@ public class LoanAdmin extends Organizations {
         super(Organizations.Type.LoanAdmin.getValue());
     }
 
-    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new LoanAdminRole());
+        return roles;
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
