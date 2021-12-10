b44492d8cfd4c9780e1a80189cfea5061e28b51d
Yash Navadiya <yashnavadiya12@gmail.com>
2021-12-09 23:18:46 -0500

    Organization MechanicOrganization

---
+++ db4odemo/src/Business/Organizations/MechanicOrg.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organizations;
+
+import Business.Role.MechanicRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sanee
+ */
+public class MechanicOrg extends Organizations{
+    public MechanicOrg() {
+        super(Organizations.Type.Mechanic.getValue());
+    }
+    
+    
+}
